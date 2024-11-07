d56addd1e28f8c5a13a3335d228c076406c842a6
VigneshCPandian <vignesh.c.pandian@gmail.com>
2024-11-07 01:30:02 +0000

    dev backend seperated

--- provider.tf
+++ provider.tf
@@ -1,13 +1,12 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "workhuman-iac-tf-state-bucket"
-    key            = "project/${var.environment}/terraform.tfstate"  # Environment-specific key
-    region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock"
-    encrypt        = true
-  }
+provider "aws" {
+  region = "us-east-1"
 }
