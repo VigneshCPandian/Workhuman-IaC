36c3a54da89d2985b21e866a0159dc2c4e9c936a
VigneshCPandian <vignesh.c.pandian@gmail.com>
2024-11-07 21:56:35 +0000

    env

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,3 +1,7 @@
 variable "vpc_cidr" {}
 variable "web_subnet_cidrs" {}
 variable "db_subnet_cidrs" {}
+variable "environment" {
+  description = "The environment for which to deploy resources (e.g., dev, staging, prod)"
+  type        = string
+}
