1e01245c09a198c4035cdc6667897965a9747b2d
VigneshCPandian <vignesh.c.pandian@gmail.com>
2024-11-07 21:30:46 +0000

    Revert "restructure"
    This reverts commit 8a7457ee2ae09839cb07e099a5424f79001b07b8.

--- modules/db_tier/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "private_subnet_ids" {}
-variable "db_instance_class" {}
-variable "db_username" {}
-variable "db_password" {}
-variable "db_sg_id" {}
-variable "vpc_id" {
-  description = "The VPC ID where the database tier will be deployed"
-  type        = string
-}
--- modules/vpc/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "vpc_cidr" {}
-variable "web_subnet_cidrs" {}
-variable "db_subnet_cidrs" {}
